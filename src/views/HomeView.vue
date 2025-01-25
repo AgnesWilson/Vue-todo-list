4b9c76348f1f8d4470c7f5eee1236baf28f0930c
AgnesWilson <agnes.wilson97@hotmail.com>
2025-01-25 17:36:44 +0100

    ✨ Added input
    Added an input-field to home-page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+
+import { ref } from 'vue';
+
+const todos = ref<string[]>([]);
+
+const todoName = ref('');
+
+function addTodo() {
+  todos.value.push(todoName.value);
+}
 </script>
 
 <template>
   <main>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo" />
+    <button @click="addTodo">Lägg till todo</button>
+
     <p>HEM</p>
   </main>
 </template>
