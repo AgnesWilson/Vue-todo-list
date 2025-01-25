2489e090b1d4ac085513b11904c7b07a038bebaf
AgnesWilson <agnes.wilson97@hotmail.com>
2025-01-25 17:43:19 +0100

    ✨ add each to do item as a component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-
 import { ref } from 'vue';
+
+import SingleTodo from '@/components/SingleTodo.vue'; 
 
 const todos = ref<string[]>([]);
 
@@ -13,21 +14,16 @@ function addTodo() {
 
 <template>
   <main>
-    <div>
-    
-    <ul v-if="todos.length > 0">
-      <li v-for="(todo, index) in todos" :key="index">
-        {{ todo }}
-      </li>
-    </ul>
-    
-    <p v-if="todos.length === 0">Du är klar med alla uppgifter!</p>
-    
-  </div>
+    <div class="my-8">
 
+      <div v-if="todos.length > 0" v-for="(todo, index) in todos" :key="index">
+        <SingleTodo :todo-text="todo" />
+      </div>
+
+      <p v-if="todos.length === 0">Du är klar med alla uppgifter!</p>
+
+    </div>
     <input type="text" v-model="todoName" @keyup.enter="addTodo" />
     <button @click="addTodo">Lägg till todo</button>
-
-    <p>HEM</p>
   </main>
 </template>
