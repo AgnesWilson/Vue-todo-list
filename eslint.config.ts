40ead2cec19edfadb07adef8678441145eda504b
AgnesWilson <agnes.wilson97@hotmail.com>
2025-01-25 18:24:54 +0100

    🚧 trying to get eslint and prettier to work
    I dont get any errors in the code from ether eslint or prettier....

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,18 +2,13 @@ import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: { browser: true } },
     rules: {
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Prevent unused variables while ignoring intentional `_` placeholders
       'no-console': 'warn', // Encourage removal of `console.log` in production code
