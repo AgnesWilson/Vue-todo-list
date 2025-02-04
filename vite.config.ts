31e4694805097b86c5f16f1b69cb69b095b376be
AgnesWilson <agnes.wilson97@hotmail.com>
2025-02-04 10:13:07 +0100

    🐛 last attempt

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-    base: '/AgnesWilson/Vue-todo-list/'
+    base: '/agneswilson/Vue-todo-list/'
 })
