db6c1c548c513be9232cd20d49647af23f05923d
AgnesWilson <agnes.wilson97@hotmail.com>
2025-02-04 10:16:01 +0100

    🐛 update againn

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-    base: '/agneswilson/Vue-todo-list/'
+    base: '/AgnesWilson/Vue-todo-list/'
 })
