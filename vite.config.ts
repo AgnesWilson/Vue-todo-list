9a312a7ffeffab65dc0a22bc1d9c7a03a42333d4
AgnesWilson <agnes.wilson97@hotmail.com>
2025-02-04 09:54:53 +0100

    🐛 Trying  new base to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-    base: '/AgnesWilson/Vue-todo-list/'
+    base: '/fed24d-arbetsmetodik-inl-2-vue-AgnesWilson/'
 })
